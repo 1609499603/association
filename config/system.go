9e980e6fbf90038c62e2f9cefb6813c30b638e2d
tyl <1609499603@qq.com>
2022-04-28 16:55:42 +0800

    test

--- config/system.go
+++ config/system.go
@@ -2,4 +2,5 @@ package config
 
 type System struct {
 	Ip2locationPath string `mapstructure:"ip2location_path" json:"ip2location_path" yaml:"ip2location_path"`
+	PageSize        int64  `mapstructure:"page_size" json:"page_size" yaml:"page_size"`
 }
