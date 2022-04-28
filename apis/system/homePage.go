22d0dfa9a94ae825b710d0f8c3afe621be9cca06
tyl <1609499603@qq.com>
2022-04-28 22:11:56 +0800

    test

--- apis/system/homePage.go
+++ apis/system/homePage.go
@@ -7,65 +7,61 @@ import (
 	"context"
 	"encoding/json"
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/cast"
 	"strconv"
 	"time"
 )
 
-func AssociationFirst(c *gin.Context) {
-	pageSize := global.ASS_CONFIG.System.PageSize
-	all, number := homePageService.AssociationNumber()
-	var i int64
-	var j int64 = 1
-	var k int64 = 0
-	m := make(map[string][]models.Association)
-	key := "all:association"
-
-	if number > pageSize {
-		for i = pageSize; i <= number; i += pageSize {
-			m["association"+strconv.FormatInt(j, 10)] = all[k:i]
-			j++
-			k += pageSize
-		}
-		if number%pageSize != 0 {
-			m["association"+strconv.FormatInt(j, 10)] = all[i-pageSize:]
-		}
-
-	} else {
-		m["association"+strconv.FormatInt(j, 10)] = all
-	}
-
-	marshal, _ := json.Marshal(m)
-
-	global.ASS_REDIS.Set(context.Background(), key, marshal, time.Hour*24*365*100)
-	response.OkWithDetailed(gin.H{
-		"pageAll": j,
-		"number":  number,
-	}, "ok", c)
-}
-
+// AssociationList 查询redis中社团信息，如果redis中没有社团信息，则查询数据库后放入redis
 func AssociationList(c *gin.Context) {
 	pageNo, _ := strconv.ParseInt(c.Query("page_no"), 10, 32)
-
-	bytes, err := global.ASS_REDIS.Get(context.Background(), "all:association").Bytes()
-	if err != nil {
-		response.Fail(c)
+	name := c.Query("name")
+	key := name
+	if name == "" {
+		key = "all"
 	}
-	m := make(map[string][]models.Association)
-	_ = json.Unmarshal(bytes, &m)
+	bytes, _ := global.ASS_REDIS.Get(context.Background(), key+":association").Bytes()
+	if len(bytes) != 0 {
+		m := make(map[string]interface{})
+		_ = json.Unmarshal(bytes, &m)
 
-	response.OkWithDetailed(m["association"+strconv.FormatInt(pageNo, 10)], "成功", c)
+		response.OkWithDetailed(gin.H{
+			"data":    m["association"+strconv.FormatInt(pageNo, 10)],
+			"pageAll": cast.ToString(m["index"]),
+			"number":  cast.ToString(m["number"]),
+		}, "成功", c)
+	} else {
+		assPutRedis(name)
+		bytes, _ := global.ASS_REDIS.Get(context.Background(), key+":association").Bytes()
+		m := make(map[string]interface{})
+		_ = json.Unmarshal(bytes, &m)
+		response.OkWithDetailed(gin.H{
+			"data":    m["association"+strconv.FormatInt(pageNo, 10)],
+			"pageAll": cast.ToString(m["index"]),
+			"number":  cast.ToString(m["number"]),
+		}, "成功", c)
+	}
 }
 
-// AssociationNameFirst 根据社团名称检索后加入缓存
-func AssociationNameFirst(c *gin.Context) {
-	name := c.Query("name")
+// 若name为空则是全部数据存入缓存，若name不为空则是检索name后存入缓存
+func assPutRedis(name string) {
+
+	var key string
+	var all []models.Association
+	var number int64
+	if name != "" {
+		key = name + ":association"
+		all, number = homePageService.AssociationName(name)
+	} else {
+		key = "all:association"
+		all, number = homePageService.AssociationNumber()
+	}
 	pageSize := global.ASS_CONFIG.System.PageSize
-	key := name + ":association"
-	m := make(map[string][]models.Association)
-	all, number := homePageService.AssociationName(name)
+
 	var i int64
 	var j int64 = 1
 	var k int64 = 0
+	m := make(map[string]interface{})
 
 	if number > pageSize {
 		for i = pageSize; i <= number; i += pageSize {
@@ -81,26 +77,9 @@ func AssociationNameFirst(c *gin.Context) {
 		m["association"+strconv.FormatInt(j, 10)] = all
 	}
 
+	m["index"] = j
+	m["number"] = number
 	marshal, _ := json.Marshal(m)
+
 	global.ASS_REDIS.Set(context.Background(), key, marshal, time.Hour*24*365*100)
-
-	response.OkWithDetailed(gin.H{
-		"pageAll": j,
-		"number":  number,
-	}, "ok", c)
-
-}
-
-func AssociationName(c *gin.Context) {
-	pageNo, _ := strconv.ParseInt(c.Query("page_no"), 10, 32)
-	name := c.Query("name")
-
-	bytes, err := global.ASS_REDIS.Get(context.Background(), name+":association").Bytes()
-	if err != nil {
-		response.Fail(c)
-	}
-	m := make(map[string][]models.Association)
-	_ = json.Unmarshal(bytes, &m)
-
-	response.OkWithDetailed(m["association"+strconv.FormatInt(pageNo, 10)], "成功", c)
 }
--- service/system/homePageService.go
+++ service/system/homePageService.go
@@ -3,19 +3,9 @@ package system
 import (
 	"association/global"
 	models "association/modules"
-	"association/modules/dto"
 )
 
 type HomePageService struct{}
-
-func (h *HomePageService) AssociationPage(pageNo, pageSize int) (page []dto.AssPage, err error) {
-
-	var a []models.Association
-
-	global.ASS_DB.Limit(pageSize).Offset(pageNo).Find(&a).Scan(&page)
-
-	return page, err
-}
 
 func (h *HomePageService) AssociationNumber() (ass []models.Association, number int64) {
 	var a []models.Association
